c07183db0df8e97469bff50524c909c526d7eb99
ciuse <ciuse@yandex.ru>
2023-02-06 19:59:35 +0700

    прикрутил данные из строки

--- vk_api.py
+++ vk_api.py
@@ -19,7 +19,7 @@ def chat_listener(token: str = token_soc):
 
         url_lp = f"https://{data_dict['response']['server']}?act=a_check&" \
                  f"key={data_dict['response']['key']}&" \
-                 f"ts={ts_number}&wait=13&mode=2&version=3"
+                 f"ts={ts_number}&wait=60&mode=2&version=3"
         resp2 = requests.get(url_lp).json()
         print(resp2)
 
@@ -27,11 +27,24 @@ def chat_listener(token: str = token_soc):
 
             for my_list in resp2['updates']:
 
-                if my_list[0] == 4 and my_list[5].lower() in ['найди пару', 'пару', 'подругу']:
-                    chat_sender(chat_id=my_list[3], mesaga=f"{get_user_first_name(user=my_list[6]['from'])} какую тебе?")
+                # if my_list[0] == 4 and my_list[5].lower() in ['найди пару', 'пару', 'подругу']:
+                #     chat_sender(chat_id=my_list[3], mesaga=f"{get_user_first_name(user=my_list[6]['from'])} какую тебе?")
+                #
+                # elif my_list[0] == 4 and my_list[5].lower() in ['красивую']:
+                #     chat_sender(chat_id=my_list[3], mesaga=f"{get_user_first_name(user=my_list[6]['from'])} ща буит!")
 
-                elif my_list[0] == 4 and my_list[5].lower() in ['красивую']:
+                if my_list[0] == 4 and my_list[5].lower() in ['найди пару', 'пару', 'подругу']:
+                    chat_sender(chat_id=my_list[3], mesaga=f"{get_user_first_name(user=my_list[6]['from'])} "
+                                                           f"укажи пол, возраст и город как показано в образце:\n"
+                                                           f"Пол: ж\nВозраст: 27\nГород: Томск")
+
+                elif my_list[0] == 4 and my_list[5].startswith('Пол:'):
                     chat_sender(chat_id=my_list[3], mesaga=f"{get_user_first_name(user=my_list[6]['from'])} ща буит!")
+                    mu_list = my_list[5].split('<br>')
+                    print(mu_list)
+                    print(mu_list[0][-1]) # пол
+                    print(mu_list[1][-2:]) # возраст
+                    print(mu_list[2][7:]) # город
 
 
 
@@ -91,7 +104,6 @@ def user_search(token: str = vk_token):
 
     resp = requests.get(url, params=par).json()
     return resp
-
 
 
 if __name__ == '__main__':
